4c659c1d8ac2242a9547ba18b1132436826d966e
gerrywu1990 <gerrywu1990@gmail.com>
2019-11-12 15:18:45 -0500

    added visual test and run on both v1 and v2

--- test/pages/loginAd.page.js
+++
@@ -1,9 +0,0 @@
-import { LoginPage } from './login.page'
-
-class LoginAdPage extends LoginPage {
-  open() {
-    browser.url('/hackathon.html?showAd=true')
-  }
-}
-
-export default new LoginAdPage()
